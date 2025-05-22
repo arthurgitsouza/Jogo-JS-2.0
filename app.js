6f4e3a28e27b22382908fdb49d6b0ce1c8cf740b
Joao Arthur Pereira de Souza <JT0075306@MATEUS.DC>
2025-05-22 11:58:07 -0300

    função return

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+let numeroSecreto = gerarNumeroAleatorio()
+
 let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Jogo do Número Secreto'
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
-function exibirTextoNaTela(campo, tag, texto){
+function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag)
     campo.innerHTML = texto
 }
 
+exibirTextoNaTela('h1', 'Jogo do Número Secreto')
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
+
 function verificarChute(){
-    console.log('Botão clicado com Sucesso!')
+    let
+    console.log(numeroSecreto)
+}
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1)
 }
