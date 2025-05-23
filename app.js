34c2ea65a9be7979fb4941d5d6d55fca17d9611f
Joao Arthur Pereira de Souza <JT0075306@MATEUS.DC>
2025-05-23 09:22:49 -0300

    up

--- app.js
+++ app.js
@@ -15,10 +15,18 @@ exibirTextoNaTela('h1', 'Jogo do Número Secreto')
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
 
 function verificarChute(){
-    let
-    console.log(numeroSecreto)
+    let chute = document.querySelector('input').value
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Parabéns, você acertou!')
+        exibirTextoNaTela('p', 'Você descobriu o número secreto!')
+    } else if (chute > numeroSecreto){
+        exibirTextoNaTela('p', `O número secreto é menor que ${chute}!`)
+    } else {
+        exibirTextoNaTela('p', `O número secreto é maior que ${chute}!`)
+    }
 }
 
 function gerarNumeroAleatorio(){
     return parseInt(Math.random() * 10 + 1)
 }
+
