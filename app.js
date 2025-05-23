56c9b93edb553fc37435e9d06fc23fb5e1bde690
Joao Arthur Pereira de Souza <JT0075306@MATEUS.DC>
2025-05-23 11:37:51 -0300

    novas fatures

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+let listaDeNumerosSorteados = [];
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 let titulo = document.querySelector('h1');
+
+function exibirMensagemInicial(){
+    console.log(listaDeNumerosSorteados);
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
@@ -8,15 +15,35 @@ function exibirTextoNaTela(tag, texto){
 }
 
 function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * 10 + 1)
+    numeroEscolhido = parseInt(Math.random() * 10 + 1);
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    }else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
 }
 
 function limparCampo(){
     document.querySelector('input').value = ''
 }
 
-exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function finalizarJogo(){
+    limparCampo()
+    document.getElementById('reiniciar').removeAttribute('disabled') // A função getElementById busca no HTML por um ID referênciado e a função removeAttribute remove o atributo relacionado a aquele id.
+    document.getElementById('chute').setAttribute('disabled', true);
+}
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio()
+    limparCampo()
+    tentativas = 1
+    exibirMensagemInicial()
+    document.getElementById('chute').removeAttribute('disabled');
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+exibirMensagemInicial()
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
@@ -24,6 +51,7 @@ function verificarChute(){
         exibirTextoNaTela('h1', 'Parabéns, você acertou!');
         let mensagemTentativas = tentativas > 1? 'tentativas' : 'tentativa';
         exibirTextoNaTela('p', `Você descobriu o número secreto com ${tentativas} ${mensagemTentativas}`);
+        finalizarJogo()
     } else if (chute > numeroSecreto){
         exibirTextoNaTela('p', `O número secreto é menor que ${chute}!`);
     } else {
@@ -31,5 +59,4 @@ function verificarChute(){
     }
     tentativas++;
     limparCampo()
-    document.getElementById('reiniciar').removeAttribute('disabled') // A função getElementById busca no HTML por um ID referênciado e a função removeAttribute remove o atributo relacionado a aquele id.
 }
