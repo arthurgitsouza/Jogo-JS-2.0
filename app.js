0fc51176789758410b2872274ebee1c435c77152
Joao Arthur Pereira de Souza <JT0075306@MATEUS.DC>
2025-05-23 09:49:01 -0300

    function

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
-let numeroSecreto = gerarNumeroAleatorio()
-
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto'
-
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 function exibirTextoNaTela(tag, texto){
-    let campo = document.querySelector(tag)
-    campo.innerHTML = texto
-}
-
-exibirTextoNaTela('h1', 'Jogo do Número Secreto')
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
-
-function verificarChute(){
-    let chute = document.querySelector('input').value
-    if (chute == numeroSecreto){
-        exibirTextoNaTela('h1', 'Parabéns, você acertou!')
-        exibirTextoNaTela('p', 'Você descobriu o número secreto!')
-    } else if (chute > numeroSecreto){
-        exibirTextoNaTela('p', `O número secreto é menor que ${chute}!`)
-    } else {
-        exibirTextoNaTela('p', `O número secreto é maior que ${chute}!`)
-    }
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
 
 function gerarNumeroAleatorio(){
     return parseInt(Math.random() * 10 + 1)
 }
 
+function limparCampo(){
+    document.querySelector('input').value = ''
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Parabéns, você acertou!');
+        let mensagemTentativas = tentativas > 1? 'tentativas' : 'tentativa';
+        exibirTextoNaTela('p', `Você descobriu o número secreto com ${tentativas} ${mensagemTentativas}`);
+    } else if (chute > numeroSecreto){
+        exibirTextoNaTela('p', `O número secreto é menor que ${chute}!`);
+    } else {
+        exibirTextoNaTela('p', `O número secreto é maior que ${chute}!`);
+    }
+    tentativas++;
+    limparCampo()
+    document.getElementById('reiniciar').removeAttribute('disabled') // A função getElementById busca no HTML por um ID referênciado e a função removeAttribute remove o atributo relacionado a aquele id.
+}
